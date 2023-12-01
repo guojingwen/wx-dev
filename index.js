4b4f1e5adceda1a930851b6a54274986d811f569
guojw <2315162186@qq.com>
2023-12-01 11:38:47 +0800

    auth

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ import sha1 from 'sha1';
 const app = new Koa();
 
 const config = {
-    token: 'wx-dev',
+    token: 'wx-dev', // 来自 接口配置信息
+
     appID: 'wx71a4ef0889d6ef46',
     appsecret: 'dba63edf28fb758d6ea829042bbed2f0'
 }
 app.use(async ctx => {
     console.log(ctx.query)
     if(ctx.method === 'GET') {
+        // 你可以这样直接返回完成微信验证签名
+       /*  if(ctx.query.echostr) {
+            ctx.body = ctx.query.echostr;
+            return
+        } */
+        // 但是不建议，因为别人可能冒充微信给你发消息
+        // 正确的验证签名如下
         const {signature, echostr, timestamp, nonce} = ctx.query
         const {token} = config
         const arr = [timestamp, nonce, token];
         const str = arr.join(' ');
         const sha1Str = sha1(str);
+        console.log('---', sha1Str, signature)
         if(sha1Str === signature) {
-            ctx.body = echostr;
-            return
+            return ctx.body = echostr;
         }
     }
     ctx.body = '微信公众号开发';
